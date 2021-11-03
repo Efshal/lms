1483e1124d8a821c070b00e7d9c9f82de2ca9626
Zain Haider <zhaider222000@gmail.com>
2021-11-03 19:49:13 +0500

    TAGS

---
+++ src/app/components/tags/tags.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FirebaseVideoService } from 'src/app/services/firebase-video.service';
+
+@Component({
+  selector: 'app-tags',
+  templateUrl: './tags.component.html',
+  styleUrls: ['./tags.component.scss'],
+})
+export class TagsComponent implements OnInit {
+  tag:string
+  courses:any
+  constructor(
+    private videoService: FirebaseVideoService,
+    private route: ActivatedRoute,
+    public router: Router,
+
+  ) 
+   {}
+
+  async ngOnInit() {
+    // this.router.routeReuseStrategy.shouldReuseRoute = () => false
+    this.route.queryParams
+      .subscribe(params => {
+        console.log(params); // { order: "popular" }
+        this.tag=params.tag
+      }
+    );
+     this.courses=await this.videoService.getTagLesson(this.tag);
+  }
+
+  async tags(tag){
+    console.log(tag)
+    // this.router.navigateByUrl('/', {skipLocationChange: true}).then(
+    // ()=>
+    this.router.navigate(['/courses/tags'], { queryParams: { tag: tag} });
+    //ask wajeeh how to reload a same url or navigate to same url with different parameter
+    this.courses=await this.videoService.getTagLesson(tag);
+  }
+   
+  //ask wajeeh about this routing
+  redirect(url) {
+    console.log(url);
+    this.videoService.setUrlFunc(url);
+    this.router.navigate(['/courses/course-overview'], { queryParams: { link: url } });
+    console.log('heree');
+  }
+}
--- src/app/courses/courses.page.ts
+++ src/app/courses/courses.page.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { FirebaseVideoService } from '../services/firebase-video.service';
 @Component({
@@ -10,32 +10,42 @@ import { FirebaseVideoService } from '../services/firebase-video.service';
 })
 export class CoursesPage implements OnInit {
   courses: any;
+  tag:string;
   constructor(
     private http: HttpClient,
     public router: Router,
-    private videoService: FirebaseVideoService
+    private videoService: FirebaseVideoService,
+    private route: ActivatedRoute
+
   ) {}
 
   ngOnInit() {
-    // this.courses = this.http.get('https://fakestoreapi.com/products');
     this.getCourse();
   }
 
-  getCourse = () =>
-    this.videoService.getCourse().subscribe(
+  getCourse =  () =>{
+     this.videoService.getCourse().subscribe(
       (res) => {
         this.courses = res;
-        // for (let i = 0; i < this.courses.length; i++) {
-        //   console.log(this.courses[i].payload.doc.id);
-        // }
+        console.log(this.courses)
       },
       (err) => console.log(err)
     );
+  }
 
   redirect(url) {
     console.log(url);
     this.videoService.setUrlFunc(url);
     this.router.navigate(['/courses/course-overview'], { queryParams: { link: url } });
     console.log('heree');
+  }
+
+  async tags(tag){
+    console.log(tag)
+    // this.getCourse()
+    // console.log(this.courses)
+    // this.courses=this.courses.filter(lesson=>lesson.payload.doc.data().tags.includes(tag))
+    // console.log(this.courses)
+    this.router.navigate(['/courses/tags'], { queryParams: { tag: tag} });
   }
 }
